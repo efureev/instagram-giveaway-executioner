a39a4fcea02a5fed5b76d702b2c384106468fcb2
madrenodriza <eth211992@gmail.com>
2020-11-15 00:47:07 -0700

    Testing InstagramAPI structure

--- heroku/index.js
+++ heroku/index.js
@@ -61,7 +61,7 @@ app.post('/instagram', function(req, res) {
   received_updates.unshift(req.body);
   comment_id = received_updates[0].entry[0].changes[0].value.comment_id;
   if(typeof comment_id !== 'undefined') {
-    status =  'We got a comment_id baby!';
+    status =  comment_id;
   }
   res.sendStatus(200);
 });
